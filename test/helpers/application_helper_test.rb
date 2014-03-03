ab7a39f0069fec661dc0ea969ac75b9711721b98
Mikhail Dieterle <MikDiet@gmail.com>
2014-03-03 12:13:33 +0300

    tets

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,4 +1,26 @@
 require 'test_helper'
 
 class PolymorphicRender::ApplicationHelperTest < ActionView::TestCase
+  include PolymorphicRender::ApplicationHelper
+
+  setup do
+    @post1 = Post.create title: 'My First Post'
+    @post2 = Post.create title: 'I am Blogger!'
+  end
+
+  test 'simple rendering' do
+    assert_equal "Title: My First Post\n", polymorphic_render(@post1)
+  end
+
+  test 'simple collection rendering' do
+    assert_equal "Title: My First Post\nTitle: I am Blogger!\n", polymorphic_render(Post.all)
+  end
+
+  test 'namespaced rendering' do
+    assert_equal "Edit: My First Post\n", polymorphic_render(@post1, :admin)
+  end
+
+  test 'namespaced collection rendering' do
+    assert_equal "Edit: My First Post\nEdit: I am Blogger!\n", polymorphic_render(Post.all, :admin)
+  end
 end
