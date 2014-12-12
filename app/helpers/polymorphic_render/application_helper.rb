ff43e147f84dca02c5269258be208cb3043367b0
Mikhail Dieterle <mikdiet@gmail.com>
2014-12-12 12:45:08 +0300

    allow to pass additional options to underling render

--- app/helpers/polymorphic_render/application_helper.rb
+++ app/helpers/polymorphic_render/application_helper.rb
@@ -1,13 +1,13 @@
 module PolymorphicRender
   module ApplicationHelper
-    def polymorphic_render(resource, suffix = nil)
+    def polymorphic_render(resource, suffix = nil, options = {})
       if resource.is_a? ActiveRecord::Base
         path = [resource.to_partial_path, *Array(suffix)].select(&:present?).join('_')
         name = File.basename(resource.to_partial_path)
-        render partial: path, object: resource, as: name
+        render options.merge(partial: path, object: resource, as: name)
       else
         capture do
-          resource.each{ |res| concat polymorphic_render(res, suffix) }
+          resource.each{ |res| concat polymorphic_render(res, suffix, options) }
         end
       end
     end
